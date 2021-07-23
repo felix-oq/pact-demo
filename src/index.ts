652fbd4e3ea3cd701920b9e878381450b86b8e32
Felix Quast <felix.quast98@gmail.com>
2021-07-23 17:34:50 +0200

    migrate project to TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-const express = require('express');
+import express, {Request, Response} from 'express';
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send( "Hello world" );
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello world");
 });
 
 app.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
